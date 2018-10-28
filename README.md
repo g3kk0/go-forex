32abe50b16c9f56425922167fd2d9e1cfecddfb0
Mark Newman <mark.newman@r3labs.io>
2018-10-28 23:28:53 +0000

    Fixed convert

--- README.md
+++ README.md
@@ -17,29 +17,31 @@ import forex "github.com/g3kk0/go-forex"
 fc := forex.NewClient()
 
 // request latest exchange rates
-latest, err := fc.Latest()
+rates, err := fc.Latest()
 if err != nil {
     log.Println(err)
 }
 
 // quote against a specific base (default EUR)
 params := map[string]string{"base": "usd"}
-latest, err := fc.Latest(params)
+rates, err := fc.Latest(params)
 if err != nil {
     log.Println(err)
 }
 
 // quote against specific symbols
 params := map[string]string{"base": "usd", "symbols": "gbp,eur"}
-latest, err := fc.Latest(params)
+rate, err := fc.Latest(params)
 if err != nil {
     log.Println(err)
 }
 
 // currency conversion
 params := map[string]string{"from": "usd", "to": "gbp", "amount": "25.00"}
-latest, err := fc.Convert(params)
+conversion, err := fc.Convert(params)
 if err != nil {
     log.Println(err)
 }
+
+fmt.Println(conversion.Result)
 ```
--- convert.go
+++ convert.go
@@ -2,7 +2,6 @@ package goforex
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -22,29 +21,22 @@ func (c *Client) Convert(params ...map[string]string) (Convert, error) {
 		return convert, err
 	}
 
-	if params[0]["from"] == "" {
+	switch {
+	case params[0]["from"] == "":
 		err := errors.New("missing 'from' parameter")
 		return convert, err
-	}
-
-	if params[0]["to"] == "" {
+	case params[0]["to"] == "":
 		err := errors.New("missing 'to' parameter")
 		return convert, err
-	}
-
-	if params[0]["amount"] == "" {
+	case params[0]["amount"] == "":
 		err := errors.New("missing 'amount' parameter")
 		return convert, err
 	}
 
-	// missing zero!!!
-	fmt.Printf("paramsAmount = %+v\n", params[0]["amount"])
 	amount, err := strconv.ParseFloat(params[0]["amount"], 64)
 	if err != nil {
 		return convert, err
 	}
-
-	fmt.Printf("amount = %+v\n", amount)
 
 	convert.From = params[0]["from"]
 	convert.To = params[0]["to"]
@@ -56,40 +48,15 @@ func (c *Client) Convert(params ...map[string]string) (Convert, error) {
 		return convert, err
 	}
 
-	fmt.Printf("rates = %+v\n", rates)
-
 	rateInt, rateDp := Ftoi(rates.Rates[strings.ToUpper(convert.To)])
 	amountInt, amountDp := Ftoi(convert.Amount)
 
-	fmt.Printf("rateInt = %+v\n", rateInt)
-	fmt.Printf("rateDp = %+v\n", rateDp)
-	fmt.Printf("amountInt = %+v\n", amountInt)
-	fmt.Printf("amountDp = %+v\n", amountDp)
-	//
+	resultInt := rateInt * amountInt
 
-	result := rateInt * amountInt
+	dp := rateDp + amountDp
+	result := Itof(resultInt, dp)
 
-	fmt.Printf("result = %T %+v\n", result, result)
-
-	result2 := Itof(result, rateDp)
-
-	fmt.Printf("result2 = %T %+v\n", result2, result2)
+	convert.Result = result
 
 	return convert, nil
-}
-
-func getMultiplier(in string) (int64, error) {
-	s := strings.Split(in, ".")
-
-	m := "1"
-	for i := 0; i < len(s[1]); i++ {
-		m = m + "0"
-	}
-
-	multiplier, err := strconv.Atoi(m)
-	if err != nil {
-		return 0, err
-	}
-
-	return int64(multiplier), nil
 }
